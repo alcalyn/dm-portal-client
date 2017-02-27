25b55eee4f0d6f3d8c16b1a6e9c259ed3f5d2277
Angular CLI <angular-cli@angular.io>
2017-02-27 12:23:14 +0100

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { DmPortalClientPage } from './app.po';
+
+describe('dm-portal-client App', () => {
+  let page: DmPortalClientPage;
+
+  beforeEach(() => {
+    page = new DmPortalClientPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
