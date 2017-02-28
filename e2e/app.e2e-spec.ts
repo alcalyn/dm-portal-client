d49b1561fdedae1bdcd4b26b6f329bd50ce70ad3
Julien Maulny <doubjulien@hotmail.fr>
2017-02-28 19:16:53 +0100

    install bootstrap4 (#1)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('dm-portal-client App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Articles');
   });
 });
